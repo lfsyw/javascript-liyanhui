cf57550078d81c8960b83a6c74eba5360b189055
张勇 <758922096@qq.com>
2018-11-28 18:00:01 +0800

    DOM加载

--- 第34章 项目：博客前端/第十节 浏览器检测/demo.js
+++ 第34章 项目：博客前端/第十节 浏览器检测/demo.js
@@ -1,69 +1,93 @@
-
-
-
-
-
 /*
-(function getState() {    //闭包
-    alert(' ');
-})();
+function addDomLoaded(fn) {
+	if (document.addEventListener) {//W3C
+		addEvent(document, 'DOMContentLoaded', function () {
+			fn();
+			removeEvent(document, 'DOMContentLoaded', arguments.callee);
+		});
+	} else {
+		var timer = null;
+		timer = setInterval(function () {
+			try {
+				document.documentElement.doScroll('left');
+				fn();
+			} catch (e) {};
+		});
+	}
+}
+
+var isReady = false;
+var timer = null;
+function doReady(fn) {
+	if (timer) clearInterval(timer);
+	if (isReady) return;
+	isReady = true;
+	fn();
+}
+
+function addDomLoaded(fn) {
+	//这种方法，目前在主流浏览器判断的都是complete，类似与onload，即图片加载后才加载
+	//用于非主流浏览器的向下兼容即可
+
+	timer = setInterval(function () {
+		if (/loaded|complete/.test(document.readyState)) { 	//loaded是部分加载，有可能只是DOM加载完毕，complete是完全加载，类似于onload
+			doReady(fn);
+		}
+	}, 1);
+
+	timer = setInterval(function () {
+		if (document && document.getElementById && document.getElementsByTagName && document.body) {
+			doReady(fn);
+		}
+	}, 1);
+}
+
+
+addDomLoaded(function () {
+	var box = document.getElementById('box');
+	alert(box.innerHTML);
+});
 
 */
 
 
+function addDomLoaded(fn) {
+    var isReady = false;
+    var timer = null;
 
-
-(function () {
-    
-    window.sys = {};              //让外部可以访问，保存浏览器信息对象
-    var ua  = navigator.userAgent.toLowerCase();  //获取浏览器信息字符串
-    var s;                        //浏览器信息数组，浏览器名称+版本
-    //alert(ua);
-   //alert(ua.match(/msie ([\d.]+)/));   //msie 7.0,7.0
-   //alert(ua.match(/firefox\/([\d.]+)/));  //firefox/63.0,63.0
-   //alert(ua.match(/chrome\/([\d.]+)/));  //chrome/69.0.3497.92,69.0.3497.92
-   //alert(ua.match(/opr\/([\d.]+)/));   //opr/56.0.3051.116,56.0.3051.116
-   //alert(ua.match(/safari\/([\d.]+)/)); 
-   //alert(ua.match(/version\/([\d.]+).*safari/));
-   
-   
-/*  
-    if ((/msie ([\d.]+)/).test(ua)) {    //IE浏览器
-        s = ua.match(/msie ([\d.]+)/);
-        sys.ie = s[1];
+    function doReady(fn) {
+        if (timer) clearInterval(timer);
+        if (isReady) return;
+        isReady = true;
+        fn();
     }
-    if ((/firefox\/([\d.]+)/).test(ua)) {   //火狐浏览器
-        s = ua.match(/firefox\/([\d.]+)/);
-        sys.firefox = s[1];
-    }
-    if ((/chrome\/([\d.]+)/).test(ua)) {    //谷歌浏览器
-        s = ua.match(/chrome\/([\d.]+)/);
-        sys.chrome = s[1];
-    } 
-    if ((/opr\/([\d.]+)/).test(ua)) {        //欧朋浏览器,新版不好判断
-        s = ua.match(/opr\/([\d.]+)/);   
-        sys.opera = s[1];
-    }
-    if ((/version\/([\d.]+).*safari/).test(ua)) {     //苹果浏览器
-        s = ua.match(/version\/([\d.]+).*safari/);
-        sys.safari = s[1];
-    }
-*/
 
-    (s = ua.match(/msie ([\d.]+)/)) ? sys.ie = s[1] :
-    (s = ua.match(/firefox\/([\d.]+)/)) ? sys.firefox = s[1] :
-    (s = ua.match(/chrome\/([\d.]+)/)) ? sys.chrome = s[1] :
-    (s = ua.match(/opr\/([\d.]+)/)) ? sys.opera = s[1] :
-    (s = ua.match(/version\/([\d.]+).*safari/)) ? sys.safari = s[1] : 0;
+    if ((sys.opera && sys.opera < 9) || (sys.firefox && sys.firefox < 3) || (sys.webkit && sys.webkit < 525)) {
+		//无论采用哪种，基本上用不着了
+		/*timer = setInterval(function () {
+			if (/loaded|complete/.test(document.readyState)) { 	//loaded是部分加载，有可能只是DOM加载完毕，complete是完全加载，类似于onload
+				doReady();
+			}
+		}, 1);*/
 
-})();
+		timer = setInterval(function () {
+			if (document && document.getElementById && document.getElementsByTagName && document.body) {
+				doReady();
+			}
+		}, 1);
+	} else if (document.addEventListener) {//W3C
+		addEvent(document, 'DOMContentLoaded', function () {
+			fn();
+			removeEvent(document, 'DOMContentLoaded', arguments.callee);
+		});
+	} else if (sys.ie && sys.ie < 9){
+		var timer = null;
+		timer = setInterval(function () {
+			try {
+				document.documentElement.doScroll('left');
+				doReady();
+			} catch (e) {};
+		}, 1);
+	}
 
-alert(sys.safari);
-
-
-
-
-//三元
-//true ? s=1 : 0;
-// (s=1) ? b=1 : (s=2) ? b=2 : (s=3) ? b=3 : 0;
-// alert(b);
+}
